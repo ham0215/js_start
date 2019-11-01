b9fcc5c81f9a4a7fc00e1869c480d7b4868647de
naoto hamada <hamada@vis-its.com>
2019-11-01 12:17:26 +0900

    Add commander

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+console.log(process.argv);
+const myModule = require("./my-module");
+console.log(myModule.foo);
+
+const program = require("commander");
+program.parse(process.argv);
+
+const filePath = program.args[0];
+console.log(filePath);
