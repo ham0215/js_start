ca825956556767784c152c1e1c59af9ff7ae8bc4
naoto hamada <hamada@vis-its.com>
2019-11-01 16:28:06 +0900

    Add md2html module

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,6 +1,6 @@
 const program = require("commander");
 const fs = require("fs");
-const marked = require("marked");
+const md2html = require("./md2html");
 
 program.usage("[options] mdfile")
 program.option("--gfm", "GFMを有効にする");
@@ -20,6 +20,6 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
         process.exit(1);
         return;
     }
-    const html = marked(file, { gfm: cliOptions.gfm });
+    const html = md2html(file, cliOptions);
     console.log(html);
 });
