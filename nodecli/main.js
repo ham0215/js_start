c376fb9ecd6f226c928b8af1b39dc198d1f45b34
naoto hamada <hamada@vis-its.com>
2019-11-01 15:38:03 +0900

    file read

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,9 +1,16 @@
-console.log(process.argv);
-const myModule = require("./my-module");
-console.log(myModule.foo);
-
 const program = require("commander");
-program.parse(process.argv);
+const fs = require("fs");
 
+program.parse(process.argv);
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+        process.exit(1);
+        return;
+    }
+    console.log(file);
+});
